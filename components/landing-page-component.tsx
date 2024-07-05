2b6661c0150818032e07304ef8f0af23764c0d53
Solideo Putra Zendrato <solidzendrato@gmail.com>
2024-07-05 12:43:58 +0200

    styling landing page

--- components/landing-page-component.tsx
+++ components/landing-page-component.tsx
@@ -14,14 +14,14 @@ const LandingPageComponent = () => {
     offset: ["0 1", "1.33 1"],
   });
 
-  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
   return (
     <div>
       <div className="h-screen flex flex-col md:flex-row justify-between">
-        <div className="w-full h-[20rem]">
-          <div className="bg-[url('/background.jpg')] bg-cover h-[20rem] pl-48 md:flex items-center shadow-2xl">
+        <div className=" w-full h-[20rem]">
+          <div className="bg-[url('/background.jpg')] bg-cover h-[30rem] pl-48 md:flex items-center shadow-2xl">
             <div className="absolute inset-0 grid-pattern"></div>
           </div>
           <div className=" h-[200px] bg-blue-700 w-[300px] md:w-[500px] mx-auto mt-[-80px] rounded-3xl">
@@ -33,29 +33,35 @@ const LandingPageComponent = () => {
               Port Data Center
             </div>
           </div>
-          <div className="bg-gray-50 md:h-[13rem] pl-4 mt-10 mb-14 space-y-3 text-center">
-            <p className="text-7xl font-serif font-bold">
-              {"Let's"} collect the data from the vessel
-            </p>
-            <div className=" text-4xl font-serif">
-              <TypewriterComponent
-                options={{
-                  strings: ["Effortlessly", "Efficiently,", "and Effectively."],
-                  autoStart: true,
-                  loop: true,
-                }}
-              />
-            </div>
-            <p className="text-3xl font-serif font-bold underline">
-              {`"to speed up and increase the productivity of the port & vessel"`}
-            </p>
+        </div>
+      </div>
+
+      <div
+        className="h-screen flex flex-col items-center justify-center text-center space-y-3"
+        
+      >
+        <div>
+          <p className="text-6xl md:text-8xl font-mono font-bold">
+            {"Let's"} collect the data from the vessel
+          </p>
+          <div className=" text-4xl font-bold">
+            <TypewriterComponent
+              options={{
+                strings: ["Effortlessly", "Efficiently,", "and Effectively."],
+                autoStart: true,
+                loop: true,
+              }}
+            />
           </div>
+          <p className="text-3xl font-serif font-bold underline">
+            {`"to speed up and increase the productivity of the port & vessel"`}
+          </p>
         </div>
       </div>
 
       <motion.div
         ref={ref}
-        className=" mt-[100px]"
+        className="h-screen"
         style={{
           scale: scaleProgress,
           opacity: scrollYProgress,
