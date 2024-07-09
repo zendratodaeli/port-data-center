1be7780199db1d0db7d44727da363da6a183f21a
Solideo Putra Zendrato <solidzendrato@gmail.com>
2024-07-09 09:38:10 +0200

    remove user from dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     >
       <html lang="en">
         <body>
-        <CheckAccess/>
+        {/* <CheckAccess/> */}
           <ToasterProvider />
           {children}
         </body>
